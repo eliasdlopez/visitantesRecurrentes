1c6ea808bb1b75206cbc72ffd053cebd7094b074
Elias <eliasdamianlopez@gmail.com>
2020-04-21 20:07:19 -0500

    ultim

--- app.js
+++ app.js
@@ -14,30 +14,55 @@ const VisitantesSchema = mongoose.Schema({
   name: String,
   count: Number
 });
+//// CREATE TABLE STATIC METHOD
+VisitantesSchema.statics.createTable = async function() {
 
+  const table = await this.find();
+
+  let tr = '';
+
+  table.forEach((item) => {
+    tr += ('<tr><td>'+item['_id']+'</td>'+
+    '<td>'+item.name+'</td>' +
+    '<td>'+item.count+'</td></tr>' );
+  });
+
+  return ('<table><thead><tr>'+
+          '<td class="text-center">Id</td>'+
+          '<td class="text-center">Name</td>'+
+          '<td class="text-center">Visits</td>'+
+          '</tr></thead><tbody>'+tr+'</tbody></table>');
+
+
+};
+
+//// MODEL
 const Visitor = mongoose.model('Visitors', VisitantesSchema);
 
+
+
+//// GET
+
 app.get('/', async (req, res) => {
+
+
   var name = req.query.name;
 
   //// CHECK IF VISITOR EXISTS
 
-  var oldVisitor = await Visitor.find({ name: name });
+  var oldVisitor = await Visitor.find({ name });
 
-  //// IF VISITOR DOESN'T EXIST, CREATE NEW ONE AND SAVE
+//// IF VISITOR DOESN'T EXIST, CREATE NEW ONE AND SAVE
 
     if (!oldVisitor.length) {
 
         const person = new Visitor ({
-        name: name,
-        count: 1
+        name,
+        count: 1,
       });
 
-      person.save((error) => {
-        if (error) {
-          return res.send(`error`);
-        }
-      });
+      await person.save();
+
 
 //// IF ANONYMOUS CREATE NEW
 
@@ -45,76 +70,23 @@ app.get('/', async (req, res) => {
         name = 'Anónimo';
 
         const person = new Visitor ({
-          name: name,
-          count: 1
+          name,
+          count: 1,
         });
 
-        person.save((error) => {
-          if (error) {
-            return res.send(`error`);
-          }
-        });
+        await person.save();
+
 
 // INCREASE COUNT IF IT EXISTS
 
   } else {
-    console.log('funcionó');
-    Visitor.updateOne({ name: name }, { $inc: {count: 1} }, console.log);
-
+    await Visitor.updateOne({ name }, { $inc: {count: 1} });
   };
 
-
-  //// CREATE TABLE
-
-  Visitor.find(function(err, table) {
-
-      if (err) return console.error(err);
-
-      let tr = '';
-
-      table.forEach((item) => {
-        tr += ('<tr><td>'+item['_id']+'</td>'+
-                   '<td>'+item.name+'</td>' +
-                   '<td>'+item.count+'</td></tr>' );
-      });
-      let result = ('<table><thead><tr>'+
-                      '<td class="text-center">'+"Id"+'</td>'+
-                      '<td class="text-center">'+"Name"+'</td>'+
-                      '<td class="text-center">'+"Visits"+'</td>'+
-                 '</tr></thead><tbody>'+tr+'</tbody></table>');
-
-      return res.send(result);
-  });
-
-
-
-
+  const table = await Visitor.createTable();
+  res.send(table);
 
 });
-
-// Visitor.find({}, (error, data) => {
-//           let tr = '';
-//           data.forEach((article) => {
-//             tr += ('<tr><td>'+article['_id']+'</td>'+
-//                        '<td>'+article.name+'</td>' +
-//                        '<td>'+article.count+'</td></tr>' );
-//           });
-//           let header = ('<table><thead><tr>'+
-//                           '<th class="text-center">'+"Id"+'</th>'+
-//                           '<th class="text-center">'+"Name"+'</th>'+
-//                           '<th class="text-center">'+"Visits"+'</th>'+
-//                      '</tr></thead>'+tr+'</table>');
-//           return  res.send(header);
-
-    // //// RETURN TABLE
-    // var fullDB = Visitor.find(function(error, visitor) {
-    //   if (error) return console.error(error);
-    //     console.log(fullDB);
-    //   });
-    //
-    //     return res.send(fullDB);
-
-
 
 
 app.listen('3000', () => console.log('visitantes Recursivo Puerto 3000'));
